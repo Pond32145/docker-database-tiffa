58cf93fff71e47c39eb1b3bb2d60f1546ebf81fc
naphat_l <pondm.32145@gmail.com>
2025-09-03 14:59:39 +0700

    commit

---
+++ pg_backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# backup.sh
+
+# Path ที่เก็บ backup
+BACKUP_DIR=/home/ubuntu/pg_backups
+
+FILENAME=paperless_$(date +%Y%m%d_%H%M%S).dump
+
+mkdir -p $BACKUP_DIR
+
+docker exec -t pg_paperless \
+  pg_dump -U paperless -d postgres -Fc \
+  > $BACKUP_DIR/$FILENAME
+
+find $BACKUP_DIR -type f -mtime +30 -name "*.dump" -delete
